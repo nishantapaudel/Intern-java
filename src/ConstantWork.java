d65ae3a56aa250bdc59c6d01fb5cd18baa3b7c18
Nishanta Paudel <60640455+nishantapaudel@users.noreply.github.com>
2025-01-17 22:53:21 +0545

    init commit

--- src/ConstantWork.java
+++ src/ConstantWork.java
@@ -1,5 +1,11 @@
 public class ConstantWork {
     public static void main(String[] args) {
 
+        final double PI = 3.14; //value of PI is always constant
+        double radius = 12;
+        double areaofcircle = PI * radius * radius ;
+
+        System.out.println("Area of Circle is: "+areaofcircle);
+
     }
 }
--- src/DataTypeWork.java
+++ src/DataTypeWork.java
@@ -1,7 +1,15 @@
 public class DataTypeWork {
     public static void main(String[] args) {
-        int num1 = 2;
-        System.out.println(num1);
+
+        byte age = 40;
+        int num1 = 12345678; //mostly used
+        double num2 = 12.12; // mostly used
+        final double PI = 3.14;
+
+        System.out.println("number is: "+age);
+        System.out.println("number 1 is: "+num1);
+        System.out.println("number 2 is: "+PI);
+        System.out.println("number 3 is: "+num2);
 
     }
 }

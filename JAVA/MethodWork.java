2fe32c218be685935ea6734bfd94b37ae61d19d6
Nishanta Paudel <nishantapaudel29@gmail.com>
2025-02-02 17:49:43 +0545

    init commit

---
+++ JAVA/MethodWork.java
@@ -0,0 +1,16 @@
+public class MethodWork {
+    public static void main(String[] args) {
+        printName();
+        printAddress();
+
+    }
+
+    public static void printName(){
+        System.out.println("Nishanta Paudel");
+    }
+
+    public static void printAddress(){
+        System.out.println("Pokhara, Nepal");
+    }
+
+}
